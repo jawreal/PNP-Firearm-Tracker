227d9374a8fe1f97c1c8be1a7196e31cfe1f1fc6
Jorell Relleve <jorellrelleve@gmail.com>
2026-01-22 13:28:31 +0800

    feat: add client dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <span>Hello World</span>
+    </div>
+  )
+}
+
+export default App;
---
+++ client/vite.config.ts
@@ -0,0 +1,30 @@
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})*/
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+}) 
