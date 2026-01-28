632e005a84c82e2cb6784e80a8636ce2c5b4b0ad
Jorell Relleve <jorellrelleve@gmail.com>
2026-01-28 10:13:52 +0800

    feat: add download func for QR code

--- client/src/components/custom/QRCodeDialog.tsx
+++ client/src/components/custom/QRCodeDialog.tsx
@@ -9,6 +9,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { useRef } from "react";
 
 interface IQRCode extends IOpenChange {
   data: IFireArm;
@@ -16,6 +17,42 @@ interface IQRCode extends IOpenChange {
 
 const QRCodeDialog = (props: IQRCode) => {
   const { open, onOpenChange, data } = props;
+  const qrRef = useRef(null);
+
+  const downloadQR = () => {
+    const svg = qrRef.current;
+    if (!svg) return;
+
+    const serializer = new XMLSerializer();
+    const svgStr = serializer.serializeToString(svg);
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+    const svgBlob = new Blob([svgStr], {
+      type: "image/svg+xml;charset=utf-8",
+    });
+
+    const url = URL.createObjectURL(svgBlob);
+
+    img.onload = () => {
+      canvas.width = 1000;
+      canvas.height = 1000;
+      ctx?.drawImage(img, 0, 0);
+      URL.revokeObjectURL(url);
+
+      const pngUrl = canvas.toDataURL("image/png");
+
+      const a = document.createElement("a");
+      a.href = pngUrl;
+      a.download = "qr-code.png";
+      a.click();
+    };
+
+    img.src = url;
+  };
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[425px] w-[calc(100%-2rem)] md:max-w-[22rem] rounded-lg">
@@ -27,6 +64,7 @@ const QRCodeDialog = (props: IQRCode) => {
         </DialogHeader>
         <div className="flex flex-col items-center gap-4 p-6">
           <QRCode
+            ref={qrRef}
             value={JSON.stringify(data)}
             size={130}
             bgColor="#ffffff"
@@ -35,7 +73,7 @@ const QRCodeDialog = (props: IQRCode) => {
         </div>
         <DialogFooter className="flex-row gap-x-3 md:gap-0 justify-end">
           <DialogClose asChild>
-            <Button type="submit" className="flex-1 h-10">
+            <Button type="submit" onClick={downloadQR} className="flex-1 h-10">
               Download as PNG
             </Button>
           </DialogClose>
