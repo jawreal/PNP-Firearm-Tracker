120201e7229296e0b8f1346cd8f15f00fe51f119
Jorell Relleve <jorellrelleve@gmail.com>
2026-02-02 22:49:42 +0800

    feat: add card in audit log table

--- client/src/pages/AuditLog.tsx
+++ client/src/pages/AuditLog.tsx
@@ -3,6 +3,10 @@ import CustomInput from "@/components/custom/CustomInput";
 import PaginationButtons from "@/components/custom/PaginationButton";
 import { Button } from "@/components/ui/button";
 import { Search, SquareArrowOutUpRight } from "lucide-react";
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card";
 
 const mockupData: IAuditLog[] = [
   {
@@ -67,16 +71,17 @@ const mockupData: IAuditLog[] = [
 
 const AuditLog = () => {
   return (
-    <div className="w-full max-w-[65rem] flex flex-col gap-y-2 pb-[4.5rem] md:pb-0">
+    <div className="w-full max-w-[65rem] flex flex-col gap-y-4 pb-[4.5rem] md:pb-0">
       <div className="flex flex-col gap-y-0">
         <h1 className="text-2xl font-bold">Audit Log</h1>
         <span className="text-gray-500 dark:text-gray-400">
           View and manage audit logs
         </span>
       </div>
-      <div className="flex mb-2">
-        <div className="flex gap-x-2 items-center ml-auto">
-          <div className="mt-1">
+      <Card className="p-0">
+      <CardContent className="p-4">
+      <div className="flex mb-5">
+                <div className="mt-1">
             {/* Search Input */}
             <CustomInput
               icon={Search}
@@ -84,7 +89,8 @@ const AuditLog = () => {
               className="max-w-sm h-9 pl-9"
               iconClassName="top-2 left-2"
             />
-          </div>
+          </div> 
+        <div className="flex gap-x-2 items-center ml-auto">
           <Button className="px-3">
             <SquareArrowOutUpRight />
             <span className="hidden md:inline">Export Logs</span>
@@ -93,6 +99,8 @@ const AuditLog = () => {
       </div>
       <AuditLogTable data={mockupData} />
       <PaginationButtons />
+     </CardContent>
+     </Card>
     </div>
   );
 };
