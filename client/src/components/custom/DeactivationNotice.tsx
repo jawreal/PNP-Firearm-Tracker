2bd2a3932909aba21803eedc37f1c35d3f1a10ec
jawreal <jorellrelleve@gmail.com>
2026-03-16 09:20:24 +1400

    feat: add auto navigate when the user field is undefined

--- client/src/components/custom/DeactivationNotice.tsx
+++ client/src/components/custom/DeactivationNotice.tsx
@@ -1,5 +1,5 @@
 import FormFooter from "@/components/custom/FormFooter";
-import { useCallback, useState, useMemo } from "react";
+import { useCallback, useState, useMemo, useEffect } from "react";
 import ResetPassword from "./ResetPassword";
 import PageLogo from "./PageLogo";
 import { Separator } from "@/components/ui/separator";
@@ -7,34 +7,41 @@ import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "@/hooks/useAuthProvider";
 import { format } from "date-fns";
-import FormatDate from "@/lib/dateFormatter"
+import FormatDate from "@/lib/dateFormatter";
 
 const DeactivationNotice = () => {
   const navigate = useNavigate();
-  const { user } = useAuthContext();
+  const { user, setUser } = useAuthContext();
   const [openForgotPass, setOpenForgotPass] = useState<boolean>(false);
 
-  const onGobackToLogin = useCallback(
-    () =>
+  const onGobackToLogin = useCallback(() => {
+    setUser(null); // reset the user
+    navigate("/auth/login", {
+      replace: true,
+    });
+  }, [navigate, setUser]);
+
+  const deactivatedAt = useMemo(() => {
+    if (!user?.deactivatedAt) {
+      return null;
+    }
+
+    const date = FormatDate(user?.deactivatedAt);
+    if (!date) {
+      return null;
+    }
+
+    return format(date, "MMM d, yyyy");
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) {
       navigate("/auth/login", {
         replace: true,
-      }),
-    [navigate],
-  );
-   
-  const deactivatedAt = useMemo(() => {
-    if(!user?.deactivatedAt){
-      return null
-    }
- 
-    const date = FormatDate(user?.deactivatedAt);
-    if(!date){
-      return null
-    }
-    
-    return format(date, "MMM d, yyyy");
-  }, [user])
-  
+      });
+    } // do not allow the user to navigate this page manually unless their account is deactivated
+  }, [user]);
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-col items-start gap-2">
@@ -53,7 +60,8 @@ const DeactivationNotice = () => {
       />
       <div className="w-full mt-4 mb-3 flex flex-col text-sm rounded-lg px-5 py-4 border border-amber-400 dark:border-amber-800 gap-y-1 gap-x-2 bg-amber-100/50 dark:bg-amber-900/70">
         <span className="text-amber-700 dark:text-gray-400">
-          {user?.deactivationReason ?? "Failed to retrieve deactivation reason. Please check your connection"}
+          {user?.deactivationReason ??
+            "Failed to retrieve deactivation reason. Please check your connection"}
         </span>
       </div>
       <Separator className="bg-gray-200/60 dark:bg-gray-700/70 mt-1 mb-3" />
@@ -62,7 +70,9 @@ const DeactivationNotice = () => {
           <span className="text-xs text-gray-500 dark:text-gray-400">
             DEACTIVATED BY
           </span>
-          <span className="text-sm capitalize">{user?.deactivatedBy ?? "User not found"}</span>
+          <span className="text-sm capitalize">
+            {user?.deactivatedBy ?? "User not found"}
+          </span>
         </div>
         <div className="flex flex-col ml-auto">
           <span className="text-xs text-gray-500 dark:text-gray-400">DATE</span>
